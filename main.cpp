2d285aeafb083d8edecd2009c88744ad3e0391af
Jade_WKA <78435515+JadeWKA@users.noreply.github.com>
2022-02-11 23:39:36 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string str1 = "lrvmnir bpr sumvbwvr jx bpr lmiwv yjeryrkbi jx qmbm wi bpr xjvni mkd ymibrut jx irhx wi bpr riirkvr jx ymbinlmtmipw utn qmumbr dj w ipmhh but bj rhnvwdmbr bpr yjeryrkbi jx bpr qmbm mvvjudwko bj yt wkbrusurbmbwjk lmird jk xjubt trmui jx ibndt";
+
+  string str2 ="wb wi kjb mk rmit bmiq bj rashmwk rmvp yjeryrkb mkd wbi iwokwxwvmkvr mkd ijyr ynib urymwk nkrashmwkrd bj ower m vjyshrbr rashmkmbwjk jkr cjnhd pmer bj lr fnmhwxwrd mkd wkiswurd bj invp mk rabrkb bpmb pr vjnhd urmvp bpr ibmbr jx rkhwopbrkrd ywkd vmsmlhr jx urvjokwgwko ijnkdhrii ijnkd mkd ipmsrhrii ipmsr w dj kjb drry ytirhx bpr xwkmh mnbpjuwbt lnb yt rasruwrkvr cwbp qmbm pmi hrxb kj djnlb bpmb bpr xjhhjcwko wi bpr sujsru msshwvmbwjk mkd wkbrusurbmbwjk w jxxru yt bprjuwri wk bpr pjsr bpmb bpr riirkvr jx jqwkmcmk qmumbr cwhh urymwk wkbmvb";
+
+
+  cout<<"\nCiphertext: "<<str1 <<endl;
+  cout<<str2<<endl;
+  cout<<"\n\nDecrpyted: "<<endl<<endl;
+
+
+for (int t=0; t<str1.length();t++){ if(str1.at(t)=='r'){
+cout << "e"; }
+else if(str1.at(t)=='f'){ cout << "b";
+}
+else if(str1.at(t)=='t'){
+cout << "j"; }
+else if(str1.at(t)=='a'){ cout << "x";
+}
+else if(str1.at(t)=='c'){
+cout << "k"; }
+else if(str1.at(t)=='e'){ cout << "v";
+}
+else if(str1.at(t)=='o'){
+cout << "b"; }
+else if(str1.at(t)=='q'){ cout << "p";
+}
+else if(str1.at(t)=='l'){
+cout << "y"; }
+else if(str1.at(t)=='t'){
+cout << "g"; }
+else if(str1.at(t)=='n'){ cout << "f";
+}
+else if(str1.at(t)=='s'){
+cout << "w"; }
+else if(str1.at(t)=='y'){ cout << "m";
+}
+else if(str1.at(t)=='x'){
+cout << "u"; }
+else if(str1.at(t)=='v'){ cout << "c";
+}
+else if(str1.at(t)=='d'){
+cout << "l"; }
+else if(str1.at(t)=='h'){ cout << "d";
+}
+else if(str1.at(t)=='u'){
+cout << "r"; }
+else if(str1.at(t)=='p'){ cout << "h";
+}
+else if(str1.at(t)=='i'){
+cout << "s"; }
+else if(str1.at(t)=='w'){ cout << "n";
+}
+else if(str1.at(t)=='j'){
+cout << "i"; }
+else if(str1.at(t)=='k'){ cout << "o";
+}
+else if(str1.at(t)=='m'){
+cout << "a"; }
+else if(str1.at(t)=='b'){ cout << "t";
+}
+else if(str1.at(t)=='r'){
+cout << "e"; }
+else if(str1.at(t)==' '){ cout << " ";
+}
+
+}
+cout<<"\n\n";
+
+for(int i=0; i<str2.length();i++){ 
+  if(str2.at(i)=='z'){
+cout << "z"; }
+else if(str2.at(i)=='f'){ cout << "q";
+}
+else if(str2.at(i)=='g'){
+cout << "j"; }
+else if(str2.at(i)=='a'){ cout << "x";
+}
+else if(str2.at(i)=='c'){
+cout << "k"; }
+else if(str2.at(i)=='e'){ cout << "v";
+}
+else if(str2.at(i)=='o'){
+cout << "b"; }
+else if(str2.at(i)=='q'){ cout << "p";
+}
+else if(str2.at(i)=='l'){
+cout << "y"; }
+else if(str2.at(i)=='t'){
+cout << "g"; }
+else if(str2.at(i)=='n'){ cout << "f";
+}
+else if(str2.at(i)=='s'){
+cout << "w"; }
+else if(str2.at(i)=='y'){ cout << "m";
+}
+else if(str2.at(i)=='x'){
+cout << "u"; }
+else if(str2.at(i)=='v'){ cout << "c";
+}
+else if(str2.at(i)=='d'){
+cout << "l"; }
+else if(str2.at(i)=='h'){ cout << "d";
+}
+else if(str2.at(i)=='u'){
+cout << "r"; }
+else if(str2.at(i)=='p'){ cout << "h";
+}
+else if(str2.at(i)=='i'){
+cout << "s"; }
+else if(str2.at(i)=='w'){ cout << "n";
+}
+else if(str2.at(i)=='j'){
+cout << "i"; }
+else if(str2.at(i)=='k'){ cout << "o";
+}
+else if(str2.at(i)=='m'){
+cout << "a"; }
+else if(str2.at(i)=='b'){ cout << "t";
+}
+else if(str2.at(i)=='r'){
+cout << "e"; }
+else if(str2.at(i)==' '){ cout << " ";
+}
+
+}
+
+}
